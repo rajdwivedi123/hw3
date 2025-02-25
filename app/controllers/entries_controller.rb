ec2cc8f3113e50c4710826cc5a138f53296dac32
Raj Dwivedi <raj.dwivedi@kellogg.northwestern.edu>
2025-02-25 03:42:07 +0000

    tables

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,27 @@
+class EntriesController < ApplicationController
+  # GET /places/:place_id/entries/new
+  def new
+    # Find the place for which the entry will be added.
+    @place = Place.find(params[:place_id])
+    @entry = @place.entries.new
+  end
+
+  # POST /places/:place_id/entries
+  def create
+    @place = Place.find(params[:place_id])
+    @entry = @place.entries.new(entry_params)
+    if @entry.save
+      # Redirect to the place’s show page after saving.
+      redirect_to place_path(@place)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Allow only the permitted parameters.
+  def entry_params
+    params.require(:entry).permit(:title, :description, :posted_on)
+  end
+end
